a41f8e9421ce772fc5ea9b8e9bed6a26e8ad3352
ShlomoV5 <shlomov@gmail.com>
2025-06-12 17:15:04 +0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(0, 43)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(109, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "<<"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(126, 43)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(111, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = ">>"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(177, Byte))
+        Me.Label1.Location = New System.Drawing.Point(4, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label1.Size = New System.Drawing.Size(236, 30)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "שם החודש, שנה"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(177, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(211, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label2.Size = New System.Drawing.Size(29, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "א"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(177, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(176, 69)
+        Me.Label3.Name = "Label3"
+        Me.Label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label3.Size = New System.Drawing.Size(29, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "ב"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(177, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(141, 69)
+        Me.Label4.Name = "Label4"
+        Me.Label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label4.Size = New System.Drawing.Size(29, 17)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "ג"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(177, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(106, 69)
+        Me.Label5.Name = "Label5"
+        Me.Label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label5.Size = New System.Drawing.Size(29, 17)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "ד"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(177, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Red
+        Me.Label6.Location = New System.Drawing.Point(71, 69)
+        Me.Label6.Name = "Label6"
+        Me.Label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label6.Size = New System.Drawing.Size(29, 17)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "ה"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label7
+        '
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(177, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Red
+        Me.Label7.Location = New System.Drawing.Point(36, 69)
+        Me.Label7.Name = "Label7"
+        Me.Label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label7.Size = New System.Drawing.Size(29, 17)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "ו"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(177, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.Red
+        Me.Label8.Location = New System.Drawing.Point(1, 69)
+        Me.Label8.Name = "Label8"
+        Me.Label8.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label8.Size = New System.Drawing.Size(29, 17)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "ש"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 7
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571!))
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(1, 89)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.TableLayoutPanel1.RowCount = 6
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(236, 159)
+        Me.TableLayoutPanel1.TabIndex = 10
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(0, 227)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(67, 22)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "היום"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(238, 250)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.RightToLeftLayout = True
+        Me.Text = "לוח שנה"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,315 @@
+﻿Imports System.Globalization
+Imports System.Windows.Forms
+
+Public Class Form1
+    Private trayIcon As NotifyIcon
+    Private contextMenu1 As ContextMenuStrip
+    Private currentHebrewYear As Integer
+    Private currentHebrewMonth As Integer ' 1-based, Tishrei=1
+    Private dayToolTips As ToolTip
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        Const WM_NCLBUTTONDOWN As Integer = &HA1
+        Const HTCAPTION As Integer = 2
+
+        If m.Msg = WM_NCLBUTTONDOWN AndAlso m.WParam.ToInt32() = HTCAPTION Then
+            ' Block form dragging
+            Return
+        End If
+
+        MyBase.WndProc(m)
+    End Sub
+
+    Private Sub ShowCalendar(sender As Object, e As EventArgs)
+        ' Position the form above the mouse (can later align to tray icon)
+        Dim mousePos = Cursor.Position
+        Me.Location = New Point(mousePos.X - Me.Width \ 2, mousePos.Y - Me.Height - 10)
+        Me.Show()
+        Me.BringToFront()
+    End Sub
+
+    Private Sub ExitApp(sender As Object, e As EventArgs)
+        trayIcon.Visible = False
+        Application.Exit()
+    End Sub
+
+    Protected Overrides Sub OnFormClosing(e As FormClosingEventArgs)
+        If e.CloseReason = CloseReason.UserClosing Then
+            e.Cancel = True
+            Me.Hide()
+        Else
+            MyBase.OnFormClosing(e)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dayToolTips = New ToolTip() With {
+        .AutoPopDelay = 5000,
+        .InitialDelay = 500,
+        .ReshowDelay = 200,
+        .ShowAlways = True
+        }
+
+        Me.ShowInTaskbar = False
+        Me.FormBorderStyle = FormBorderStyle.FixedToolWindow ' shows X, no minimize/maximize
+        Me.ControlBox = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.StartPosition = FormStartPosition.Manual
+        Me.Visible = False
+
+        ' Set up tray icon
+        trayIcon = New NotifyIcon()
+        trayIcon.Icon = SystemIcons.Information
+        trayIcon.Visible = True
+
+        ' Context menu
+        contextMenu1 = New ContextMenuStrip()
+        contextMenu1.Items.Add("הצג לוח", Nothing, AddressOf ShowCalendar)
+        contextMenu1.Items.Add("יציאה", Nothing, AddressOf ExitApp)
+        trayIcon.ContextMenuStrip = contextMenu1
+
+        ' Set Hebrew tooltip
+        Dim hc As New HebrewCalendar()
+        Dim today = Date.Today
+
+        Dim hYear = hc.GetYear(today)
+        Dim hMonth = hc.GetMonth(today)
+        Dim hDay = hc.GetDayOfMonth(today)
+
+        Dim hebrewDate = $"{IntToHebrewDay(hDay)} {GetHebrewMonthName(hMonth, hYear)} {IntToHebrewYear(hYear)}"
+        trayIcon.Text = $"היום: {hebrewDate}".Substring(0, Math.Min(63, $"היום: {hebrewDate}".Length))
+
+        ' Init calendar state
+        currentHebrewYear = hYear
+        currentHebrewMonth = hMonth
+
+        DrawCalendar(0)
+        DrawCalendar(0)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DrawCalendar(1)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DrawCalendar(-1)
+    End Sub
+
+    Private Sub DrawCalendar(Optional monthOffset As Integer = 0)
+        Dim hc As New HebrewCalendar()
+
+        ' Adjust month/year with rollover
+        currentHebrewMonth += monthOffset
+        Do While True
+            Dim isLeap = hc.IsLeapYear(currentHebrewYear)
+            Dim maxMonth = If(isLeap, 13, 12)
+
+            If currentHebrewMonth < 1 Then
+                currentHebrewYear -= 1
+                currentHebrewMonth += If(hc.IsLeapYear(currentHebrewYear), 13, 12)
+            ElseIf currentHebrewMonth > maxMonth Then
+                currentHebrewMonth -= maxMonth
+                currentHebrewYear += 1
+            Else
+                Exit Do
+            End If
+        Loop
+
+        ClearCalendarGrid()
+
+        Dim daysInMonth = hc.GetDaysInMonth(currentHebrewYear, currentHebrewMonth)
+        Dim firstDay = hc.ToDateTime(currentHebrewYear, currentHebrewMonth, 1, 0, 0, 0, 0)
+        Dim startDayOfWeek = CInt(firstDay.DayOfWeek)
+
+        ' Label1 = title
+        Dim monthName = GetHebrewMonthName(currentHebrewMonth, currentHebrewYear)
+        Dim yearText = IntToHebrewYear(currentHebrewYear)
+        Label1.Text = $"{monthName}, {yearText}"
+
+        ' Fill grid
+        For day = 1 To daysInMonth
+            Dim gDate = hc.ToDateTime(currentHebrewYear, currentHebrewMonth, day, 0, 0, 0, 0)
+            Dim dow = CInt(gDate.DayOfWeek)
+            Dim row = (day + startDayOfWeek - 1) \ 7
+
+            Dim lbl As New Label With {
+            .Text = IntToHebrewDay(day),
+            .TextAlign = ContentAlignment.MiddleCenter,
+            .Dock = DockStyle.Fill,
+            .Font = New Font("Segoe UI", 10, FontStyle.Bold),
+            .Margin = New Padding(1)
+}
+
+            dayToolTips.SetToolTip(lbl, gDate.ToString("dd/MM/yyyy"))
+
+            ' Highlight today's date
+            Dim today As Date = Date.Today
+            If gDate.Date = today.Date Then
+                lbl.BackColor = Color.LightGoldenrodYellow
+                lbl.BorderStyle = BorderStyle.FixedSingle
+            End If
+
+            TableLayoutPanel1.Controls.Add(lbl, dow, row)
+        Next
+    End Sub
+
+    Private Sub ClearCalendarGrid()
+        For i = TableLayoutPanel1.Controls.Count - 1 To 0 Step -1
+            Dim ctrl = TableLayoutPanel1.Controls(i)
+            If TypeOf ctrl Is Label Then
+                TableLayoutPanel1.Controls.RemoveAt(i)
+                ctrl.Dispose()
+            End If
+        Next
+    End Sub
+
+    Private Function GetHebrewMonthName(dotNetMonth As Integer, year As Integer) As String
+        Dim isLeap = New HebrewCalendar().IsLeapYear(year)
+        Dim monthsRegular = {
+            "תשרי", "חשון", "כסלו", "טבת", "שבט", "אדר",
+            "ניסן", "אייר", "סיון", "תמוז", "אב", "אלול"
+        }
+        Dim monthsLeap = {
+            "תשרי", "חשון", "כסלו", "טבת", "שבט", "אדר א",
+            "אדר ב", "ניסן", "אייר", "סיון", "תמוז", "אב", "אלול"
+        }
+
+        If isLeap Then
+            Return monthsLeap(dotNetMonth - 1)
+        Else
+            Return monthsRegular(dotNetMonth - 1)
+        End If
+    End Function
+
+    Private Function IntToHebrewDay(n As Integer) As String
+        Dim ones() = {"א", "ב", "ג", "ד", "ה", "ו", "ז", "ח", "ט"}
+        Dim tens() = {"י", "כ", "ל", "מ", "נ", "ס", "ע", "פ", "צ"}
+
+        If n <= 10 Then
+            If n = 10 Then Return "י"
+            Return $"{ones(n - 1)}"
+        ElseIf n = 15 Then
+            Return "טו"
+        ElseIf n = 16 Then
+            Return "טז"
+        ElseIf n < 20 Then
+            Return $"י{ones(n - 11)}"
+        ElseIf n Mod 10 = 0 Then
+            Return $"{tens(n \ 10 - 1)}"
+        Else
+            Return $"{tens(n \ 10 - 1)}{ones(n Mod 10 - 1)}"
+        End If
+    End Function
+
+    Private Function IntToHebrewYear(year As Integer) As String
+        If year <= 0 Then Return ""
+
+        Dim map As New Dictionary(Of Integer, String) From {
+            {1, "א"}, {2, "ב"}, {3, "ג"}, {4, "ד"}, {5, "ה"},
+            {6, "ו"}, {7, "ז"}, {8, "ח"}, {9, "ט"},
+            {10, "י"}, {20, "כ"}, {30, "ל"}, {40, "מ"}, {50, "נ"},
+            {60, "ס"}, {70, "ע"}, {80, "פ"}, {90, "צ"},
+            {100, "ק"}, {200, "ר"}, {300, "ש"}, {400, "ת"},
+            {500, "תק"}, {600, "תר"}, {700, "תש"}, {800, "תת"}, {900, "תתק"}
+        }
+
+        ' Handle thousands prefix
+        If year >= 1000 Then
+            Dim thousands = year \ 1000
+            Dim remainder = year Mod 1000
+            Dim prefix As String = If(map.ContainsKey(thousands), map(thousands) & "׳", "")
+            Return prefix & IntToHebrewYear(remainder)
+        End If
+
+        ' Special: טו / טז
+        If year = 15 Then Return "טו"
+        If year = 16 Then Return "טז"
+
+        ' Find largest key less than or equal to year
+        Dim key = map.Keys.Where(Function(k) k <= year).OrderByDescending(Function(k) k).FirstOrDefault()
+
+        If key = 0 Then Return ""
+        Return map(key) & IntToHebrewYear(year - key)
+    End Function
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim hc As New HebrewCalendar()
+        Dim today = Date.Today
+        currentHebrewYear = hc.GetYear(today)
+        currentHebrewMonth = hc.GetMonth(today)
+        DrawCalendar(0)
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        Dim picker As New FormMonthPicker(currentHebrewYear, currentHebrewMonth)
+        AddHandler picker.MonthSelected, AddressOf SetSelectedMonth
+        picker.Location = Cursor.Position
+        picker.Show()
+    End Sub
+
+    Private Sub SetSelectedMonth(year As Integer, month As Integer)
+        currentHebrewYear = year
+        currentHebrewMonth = month
+        DrawCalendar(0)
+    End Sub
+End Class
+
+Public Class FormMonthPicker
+    Inherits Form
+
+    Public Event MonthSelected(hebrewYear As Integer, hebrewMonth As Integer)
+
+    Private yearCombo As ComboBox
+    Private monthCombo As ComboBox
+    Private confirmButton As Button
+    Private hc As New HebrewCalendar()
+
+    Public Sub New(currentYear As Integer, currentMonth As Integer)
+        Me.FormBorderStyle = FormBorderStyle.FixedToolWindow
+        Me.StartPosition = FormStartPosition.Manual
+        Me.ShowInTaskbar = False
+        Me.Width = 200
+        Me.Height = 150
+
+        yearCombo = New ComboBox With {.DropDownStyle = ComboBoxStyle.DropDownList, .Left = 10, .Top = 10, .Width = 170}
+        monthCombo = New ComboBox With {.DropDownStyle = ComboBoxStyle.DropDownList, .Left = 10, .Top = 40, .Width = 170}
+        confirmButton = New Button With {.Text = "אישור", .Left = 10, .Top = 80, .Width = 170}
+
+        For y = 5700 To 5800
+            yearCombo.Items.Add(y)
+        Next
+        yearCombo.SelectedItem = currentYear
+
+        AddHandler yearCombo.SelectedIndexChanged, AddressOf UpdateMonthList
+        AddHandler confirmButton.Click, AddressOf ConfirmSelection
+
+        Me.Controls.Add(yearCombo)
+        Me.Controls.Add(monthCombo)
+        Me.Controls.Add(confirmButton)
+
+        UpdateMonthList(Nothing, Nothing)
+        monthCombo.SelectedIndex = currentMonth - 1
+    End Sub
+
+    Private Sub UpdateMonthList(sender As Object, e As EventArgs)
+        If yearCombo.SelectedItem Is Nothing Then Return
+        Dim year As Integer = CInt(yearCombo.SelectedItem)
+        Dim isLeap = hc.IsLeapYear(year)
+
+        monthCombo.Items.Clear()
+        Dim months = If(isLeap,
+            {"תשרי", "חשון", "כסלו", "טבת", "שבט", "אדר א", "אדר ב", "ניסן", "אייר", "סיון", "תמוז", "אב", "אלול"},
+            {"תשרי", "חשון", "כסלו", "טבת", "שבט", "אדר", "ניסן", "אייר", "סיון", "תמוז", "אב", "אלול"}
+        )
+
+        monthCombo.Items.AddRange(months)
+    End Sub
+
+    Private Sub ConfirmSelection(sender As Object, e As EventArgs)
+        If yearCombo.SelectedItem Is Nothing OrElse monthCombo.SelectedIndex = -1 Then Return
+        Dim y = CInt(yearCombo.SelectedItem)
+        Dim m = monthCombo.SelectedIndex + 1
+        RaiseEvent MonthSelected(y, m)
+        Me.Close()
+    End Sub
+End Class
